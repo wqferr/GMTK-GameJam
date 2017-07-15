1953562f1c711b5a00ede19c76cc460411ad3452
William Quelho Ferreira <wqferr@gmail.com>
2017-07-14 23:13:21 -0300

    Spawn enemies

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+	public EnemySpawner spawner;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.Translate (Vector3.left * spawner.enemySpeed * Time.fixedDeltaTime);
+		if (transform.position.x < spawner.despawnX)
+			Destroy (gameObject);
+	}
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public float spawnRadius;
+	public GameObject enemyPrefab;
+	public float enemySpeed;
+	public float despawnX;
+
+	public float spawnInterval;
+	float timeToSpawn;
+
+	// Use this for initialization
+	void Start () {
+		timeToSpawn = spawnInterval;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		timeToSpawn -= Time.deltaTime;
+		if (timeToSpawn < 0) {
+			timeToSpawn = spawnInterval;
+			Spawn ();
+		}
+	}
+
+	void Spawn() {
+		float r = (2 * Random.value - 1) * spawnRadius;
+		GameObject obj = Instantiate (enemyPrefab);
+		obj.transform.parent = transform;
+		obj.transform.position = transform.position;
+		obj.transform.Translate(Vector3.up * r);
+		obj.GetComponent<EnemyController> ().spawner = this;
+	}
+}
