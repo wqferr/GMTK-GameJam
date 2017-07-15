c5768425673ad96e72c218bd69858dc09126ef2f
murilo <theodoro.murilo44@gmail.com>
2017-07-14 23:58:37 -0300

    cleaning newlines

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -7,7 +7,8 @@ public class EnemyController : MonoBehaviour {
 	public EnemySpawner spawner;
 	
 	// Update is called once per frame
-	void FixedUpdate () {
+	void FixedUpdate ()
+	{
 		transform.Translate (Vector3.left * spawner.enemySpeed * Time.fixedDeltaTime);
 		if (transform.position.x < spawner.despawnX)
 			Destroy (gameObject);
